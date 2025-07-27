cf0b166e131e507ffb641c2fb1d2a46f86fe552c
Pratik <prsatpute2005@gmail.com>
2025-07-27 15:53:22 +0530

    day-6 update recording page section

--- src/app/(root)/schedule/page.tsx
+++ src/app/(root)/schedule/page.tsx
@@ -1,5 +1,18 @@
-export default function schedule (){
-    return(
-        <h1>schedule page</h1>
-    )
+"use client";
+
+import LoaderUI from "@/components/LoaderUI";
+import { useUserRole } from "@/hooks/useUserRole";
+import { useRouter } from "next/navigation";
+import InterviewScheduleUI from "./InterviewScheduleUI";
+
+function SchedulePage() {
+  const router = useRouter();
+
+  const { isInterviewer, isLoading } = useUserRole();
+
+  if (isLoading) return <LoaderUI />;
+  if (!isInterviewer) return router.push("/");
+
+  return <InterviewScheduleUI />;
 }
+export default SchedulePage;
